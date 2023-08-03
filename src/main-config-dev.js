afcda71814353b725cffa95088d7ff688c03dd79
gehoule <36653517+gehoule@users.noreply.github.com>
2023-08-03 16:15:54 -0400

    Update main-config-dev.js with dev URLs

--- src/main-config-dev.js
+++ src/main-config-dev.js
@@ -1,2 +1,2 @@
-const PYGEOAPI_URL_ROOT = "https://czs-pygeoapi.ddr-dev.services.geo.ca";
-const QGIS_SERVICE_URL_ROOT = "https://qgis-stage.services.geo.ca/dev/";
+const PYGEOAPI_URL_ROOT = "https://api.czs-dev.services.geo.ca";
+const QGIS_SERVICE_URL_ROOT = "https://qgis-dev.services.geo.ca/ows/";
